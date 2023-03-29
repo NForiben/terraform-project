24afd420fb1f8a25ba1030e9523bac01439d6c50
NForiben <118775486+NForiben@users.noreply.github.com>
2023-03-29 12:19:52 -0400

    Delete bucket_encryption directory

--- bucket_encryption/kms_key.tf
+++
@@ -1,4 +0,0 @@
- resource "aws_kms_key" "s3_bucket_key" {
-  description = "kms key for the S3 bucket"
-  deletion_window_in_days = 10
-}
--- bucket_encryption/random_integer.tf
+++
@@ -1,4 +0,0 @@
-resource "random_integer" "bucket_number" {
-  min = 1000
-  max = 9999
-  }
